ffe35ea41bfe819a724d07ad62378a6067343383
ANAS <flashycoderch@gmail.com>
2024-07-23 15:34:00 +0500

    Side-Bar using html css javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const sidebar = document.getElementById("side")
+const span = document.querySelectorAll("span");
+const btn = document.getElementsByTagName("button");
+
+function myFun(){
+    var menu = document.getElementById("menu-btn");
+    menu.classList.toggle("#check:checked");
+}
